ef1a5c8f918e04698fcfd2f025e93a8a374fc352
Alaor S B Junior <89108520+alaorjunior@users.noreply.github.com>
2025-10-20 23:30:12 -0300

    Inserido componentes no header e footer

---
+++ PI_2sem/src/components/footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            {/* Título do Protótipo */}
+            <div>Protótipo — Integração com API SOF-v4</div>
+            
+            {/* Nota Acadêmica */}
+            <div className="small">Feito para demonstração acadêmica</div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ PI_2sem/src/components/header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            {/* Primeira div: Títulos */}
+            <div>
+                <h1>Consulta Orçamentária (protótipo) — SOF-v4</h1>
+                <div className="small">Consuma dados públicos da Prefeitura para visualizar despesas por órgão</div>
+            </div>
+
+            {/*Controles e Tema */}
+            <div className="controls">
+                <div 
+                    className="card" 
+                    
+                    style={{ display: 'flex', alignItems: 'center', gap: '8px' }}
+                >
+                    <label 
+                        className="small" 
+                        style={{ marginRight: '6px' }}
+                    >
+                        Tema:
+                    </label>
+                    <div className="badge">Transparência Orçamentária</div>
+                    <button 
+                        id="toggleTheme" 
+                        className="secondary" 
+                        style={{ marginLeft: '12px' }}
+                    >
+                        Alternar Tema
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
