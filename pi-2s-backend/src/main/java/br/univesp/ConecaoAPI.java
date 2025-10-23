5e7cc5da051ffced0671ae741111e5207a91266e
Oliveira_Ju <107575747+J0li@users.noreply.github.com>
2025-10-22 22:26:42 -0300

    continuando conexão API

--- pi-2s-backend/src/main/java/br/univesp/ConecaoAPI.java
+++ pi-2s-backend/src/main/java/br/univesp/ConecaoAPI.java
@@ -1,15 +1,23 @@
 package br.univesp;
 
-import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import static jdk.internal.net.http.HttpRequestImpl.USER_AGENT;
+import com.sun.net.httpserver.Request;
+import class br/univesp/ParamsHeaders.java;
 
 public class ConecaoAPI {
-    public static void main(String[] args) {
+    public record User() {}
+
+    User user = new User();
+    static String userJson = mapper.writeValueAsString(user);
+
+    public static void main(String[] args) throws IOException {
 
         URL url = new URL("http://gateway.apilib.prefeitura.sp.gov.br/sf/sof/v4/orgaos") ;
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -17,22 +25,37 @@ public class ConecaoAPI {
         con.setRequestProperty("User-Agent", USER_AGENT);
         int responseCode = con.getResponseCode();
 
-        isSuccessful.status = 200;
-
         Request request = new Request.Builder()
                 .url(url)
                 .build(ParamsHeaders);
 
-        try (Response response = client.newCall(request).execute()) {
+        if(responseCode == HttpURLConnection.HTTP_OK){
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLinha;
+            StringBuffer response = new StringBuffer();
 
-            String userJson = ExampleUtils.toJson(user);
-
-            if (response.isSuccessful()) {
-                String responseData = response.body().string();
-                return responseData.JSON().get('IstOrgaos' + []);
-            } else {
-                System.out.println("Erro HTTP" + responseData_code);
+            while ((inputLinha = in.readLine()) != null ){
+                response.append(inputLinha);
             }
+            in.close();
+
+            System.out.println(response.toString());
+
+        }else {
+            System.out.println("Conexão não funcionou");
+        }
+
+        try {
+            HttpURLConnection httpURLConnection = (httpURLConnection) url.openConnection();
+            httpURLConnection.setRequestProperty("Content-Type", "application/json");
+
+                try (OutputStream outputStream = httpURLConnection.getOutputStream()) {
+                    outputStream.write(userJson.getBytes(StandardCharsets.UTF_8));
+                    outputStream.flush();
+                }
+
+            System.out.println("HTTP status:" + responseCode);
+
         } catch (Exception e) {
             System.out.println("Erro" + e);
         }
