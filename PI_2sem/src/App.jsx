292f059bbc8ed1096632b4b8def4a304532907d2
Oliveira_Ju <107575747+J0li@users.noreply.github.com>
2025-10-26 18:44:41 -0300

    importação do front

--- PI_2sem/src/App.jsx
+++ PI_2sem/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import 'src\styles\index.css'
 import Header from './components/header'
 import Footer from './components/footer'
 import Card from './components/card'
