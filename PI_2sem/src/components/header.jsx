f3d24447510804471f6cae1ce86eeb9868dea99b
Alaor S B Junior <89108520+alaorjunior@users.noreply.github.com>
2025-11-11 13:17:16 -0300

    Ajustado header.jsx ocupando 100%

--- PI_2sem/src/components/header.jsx
+++ PI_2sem/src/components/header.jsx
@@ -4,7 +4,8 @@ export default function Header() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <header className="headerContainer container" role="banner">
+    // Removido apenas "container" para o fundo ocupar toda a largura
+    <header className="headerContainer" role="banner">
       <div className="header-center">
         <h1 className="site-title">Portal de Transparência Pública</h1>
         <div className="theme-switcher">
