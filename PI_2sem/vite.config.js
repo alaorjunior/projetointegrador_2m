fdc60b59ba421f3e03bb4f54f0eceee9a7810984
Alaor S B Junior <89108520+alaorjunior@users.noreply.github.com>
2025-11-12 23:53:53 -0300

    Build para GitHub Pages

--- PI_2sem/vite.config.js
+++ PI_2sem/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/projetointegrador_2m/", // 👈 adiciona essa linha
   css: {
     postcss: "./postcss.config.js", // ✅ Garante que o Vite use o PostCSS correto
   },
