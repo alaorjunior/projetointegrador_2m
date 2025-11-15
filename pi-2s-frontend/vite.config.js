6bfd53ca99d8f74d1f72ae077224b15f57c0f5f0
Alaor S B Junior <89108520+alaorjunior@users.noreply.github.com>
2025-11-15 17:11:57 -0300

    Atualiza vite.config e prepara deploy para GitHub Pages

--- pi-2s-frontend/vite.config.js
+++ pi-2s-frontend/vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react-swc'
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
-  // ❗ Para Vercel, NUNCA use base com nome de repositório
-  // base: "/projetointegrador_2m/",
-
-  css: {
-    postcss: "./postcss.config.js",
-  },
-
-  server: {
-    port: 5173,
-    open: true,
-  }
-});
+  base: '/projetointegrador_2m/', // NOME EXATO DO SEU REPOSITÓRIO
+})
