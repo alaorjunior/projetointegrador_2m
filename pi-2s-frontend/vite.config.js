2def9c418de0c5e7fdc29ba04e121f1624c55f56
Alaor S B Junior <89108520+alaorjunior@users.noreply.github.com>
2025-11-15 16:27:29 -0300

    mudança vite.config.js tajustando plugins

--- pi-2s-frontend/vite.config.js
+++ pi-2s-frontend/vite.config.js
@@ -1,33 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/projetointegrador_2m/", // <--- coloque aqui o nome do seu repositório
+
+  // ❗ Para Vercel, NUNCA use base com nome de repositório
+  // base: "/projetointegrador_2m/",
+
   css: {
-    postcss: "./postcss.config.js", // ✅ Garante que o Vite use o PostCSS correto
+    postcss: "./postcss.config.js",
   },
+
   server: {
     port: 5173,
-    open: true, // ✅ Abre o navegador automaticamente ao rodar npm run dev
-  },
-  plugins: [
-    react({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-      },
-    }),
-
-    reactRouter(),
-    babel({
-      filter: /\.[jt]sx?$/,
-      babelConfig: {
-        presets: ["@babel/preset-typescript"], // if you use TypeScript
-        plugins: [
-          ["babel-plugin-react-compiler", ReactCompilerConfig],
-        ],
-      },
-    }),
-  ],
+    open: true,
+  }
 });
